f62e4c0d8e8b78e2859e55a77ba9f77712d772bf
Nadia Gamula <nadia.zakharova@gmail.com>
2020-07-01 11:44:47 +0300

    test: add course

---
+++ precache-manifest.209c6b2c7e7dcabb7f18c3a09a53a2b8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4d4c1e6d26860962d7ff92b9a7f813",
+    "url": "./index.html"
+  },
+  {
+    "revision": "42d65b1f6f762e0a0f49",
+    "url": "./static/css/4.0c3ad388.chunk.css"
+  },
+  {
+    "revision": "0f6bf7dff9077dc31e2d",
+    "url": "./static/css/main.c47de851.chunk.css"
+  },
+  {
+    "revision": "5130bd02deb79e0de160",
+    "url": "./static/js/0.9f6ef157.chunk.js"
+  },
+  {
+    "revision": "6e68448eb024dfd70c62",
+    "url": "./static/js/1.aae8e2dd.chunk.js"
+  },
+  {
+    "revision": "42d65b1f6f762e0a0f49",
+    "url": "./static/js/4.6209e96d.chunk.js"
+  },
+  {
+    "revision": "f5ffe62bb4dcd814606253bfe72a23ef",
+    "url": "./static/js/4.6209e96d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b207f9f520f247da8342",
+    "url": "./static/js/5.c7eb0e3f.chunk.js"
+  },
+  {
+    "revision": "b66c2d8553cfa27299f3",
+    "url": "./static/js/6.702d1aa8.chunk.js"
+  },
+  {
+    "revision": "aac26ef126428487de06",
+    "url": "./static/js/7.f59a53dc.chunk.js"
+  },
+  {
+    "revision": "0f6bf7dff9077dc31e2d",
+    "url": "./static/js/main.382a62d6.chunk.js"
+  },
+  {
+    "revision": "e3c5501ea25761950586",
+    "url": "./static/js/runtime-main.67d228ab.js"
+  },
+  {
+    "revision": "999a3e680dac7ec284ea0564fcdc4736",
+    "url": "./static/media/custom-icons.999a3e68.woff"
+  },
+  {
+    "revision": "a8204812227c872d438bfd06d78926bc",
+    "url": "./static/media/custom-icons.a8204812.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
